ced367ac4acd5c6595066ec8185f054867b7805c
KJY93 <38803618+KJY93@users.noreply.github.com>
2020-04-10 21:57:58 +0800

    Configure Vue router to toggle between different pages in the application

--- src/main.js
+++ src/main.js
@@ -1,8 +1,39 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import components
+import Vue from 'vue' 
+import Router from 'vue-router'
+import App from './App'
+import ReadMe from './components/ReadMe'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
 
-Vue.config.productionTip = false
+// setup routers
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/readme/:username/:id',
+      name:'readme', 
+      props: true,
+      component: ReadMe
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: ReadMe
+    },
+    { path: '*', component: NotFound }  
+
+  ],
+   mode: 'history'
+ })
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
