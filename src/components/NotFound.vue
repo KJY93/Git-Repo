29af1c50eeb63982b226cb0429ae79850c0b19a9
KJY93 <38803618+KJY93@users.noreply.github.com>
2020-04-12 09:45:10 +0800

    added NotFound.vue file to handle page not found

---
+++ src/components/NotFound.vue
@@ -0,0 +1,29 @@
+Untitled-2.vue<template>
+  <div id="not-found-container">
+    <img src="~@/assets/404-error.svg" alt="Image Not Found">
+    <footer class="footer-container">
+        <small>Copyright <i class="far fa-copyright"></i> 2020 by JY</small>
+    </footer>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  img {
+    max-width: 100%;
+  }
+
+  #nav {
+    margin-botton: 10px;
+  }
+
+  #not-found-container {
+    background: #e8e8e8;
+  } 
+
+  a:hover {
+    color: #7fffd4;
+  }
+</style>
